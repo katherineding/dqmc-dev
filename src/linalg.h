fa273eb89eed2f13a903c5704dfa3b4795470f9c
Jixun Ding <jxding@stanford.edu>
2022-02-02 00:00:07 -0800

    test cublas example

--- src/linalg.h
+++ src/linalg.h
@@ -1,164 +1,184 @@
 #pragma once
 
-//#include <mkl.h>
+/**	
+ * Interface of DQMC code with CUBLAS and CUSOLVER library code.
+ * Define away d
+*/
 #include "util.h"
 
 #include <cublas_v2.h>
 #include <cuda_runtime.h>
 
 #ifdef USE_CPLX
-	#define cast(p) (MKL_Complex16 *)(p)
-	#define ccast(p) (const MKL_Complex16 *)(p)
+	#define cast(p) (cuDoubleComplex *)(p)
+	#define ccast(p) (const cuDoubleComplex *)(p)
 #else
 	#define cast(p) (p)
 	#define ccast(p) (p)
 #endif
 
-// general matrix-matrix multiplication: Level 3 BLAS
-static inline void xgemm(cublasHandle_t handle, const char *transa, const char *transb,
-		const int m, const int n, const int k,
-		const num alpha, const num *a, const int lda,
-		const num *b, const int ldb,
-		const num beta, num *c, const int ldc)
-{
-#ifdef USE_CPLX
-	cublasZgemm(
-#else
-	cublasDgemm(
-#endif
-	handle, transa, transb, &m, &n, &k,
-	ccast(&alpha), ccast(a), &lda, ccast(b), &ldb,
-	ccast(&beta), cast(c), &ldc);
-}
+// // general matrix-matrix multiplication: Level 3 BLAS
+// static inline cublasStatus_t xgemm(
+// 		cublasHandle_t handle, 
+// 		cublasOperation_t transa, 
+// 		cublasOperation_t transb,
+// 		const int m, const int n, const int k,
+// 		const num *alpha, /* host or device pointer */
+// 		const num *a, 
+// 		const int lda,
+// 		const num *b, 
+// 		const int ldb,
+// 		const num *beta, /* host or device pointer */
+// 		num *c, 
+// 		const int ldc)
+// {
+// #ifdef USE_CPLX
+// 	cublasZgemm(
+// #else
+// 	cublasDgemm(
+// #endif
+// 	handle, transa, transb, m, n, k,
+// 	alpha, a, lda, b, ldb, beta, c, ldc);
+// }
 
 // general matrix-vector product, Level 2 BLAS
-static inline void xgemv(cublasHandle_t handle, const char *trans, const int m, const int n,
-		const num alpha, const num *a, const int lda,
-		const num *x, const int incx,
-		const num beta, num *y, const int incy)
-{
-#ifdef USE_CPLX
-	zgemv(
-#else
-	dgemv(
-#endif
-	handle, trans, &m, &n,
-	ccast(&alpha), ccast(a), &lda, ccast(x), &incx,
-	ccast(&beta), cast(y), &incy);
-}
-
-// triangular matrix - general matrix product, level 3 BLAS
-static inline void xtrmm(cublasHandle_t handle, const char *side, const char *uplo, const char *transa, const char *diag,
+// y <- alpha * A * x + beta * y when trans = 'N' or 'n'
+static inline cublasStatus_t xgemv(cublasHandle_t handle,
+		const cublasOperation_t trans, 
 		const int m, const int n,
-		const num alpha, const num *a, const int lda,
-		num *b, const int ldb)
+		const num *alpha, /* host or device pointer */ 
+		const num *A, /* device */ 
+		const int lda,
+		const num *x, /* device */
+		const int incx, 
+		const num *beta, /* host or device pointer */
+		num *y,       /* device */
+		const int incy)
 {
 #ifdef USE_CPLX
-	ztrmm(
+	cublasZgemv(
 #else
-	dtrmm(
+	cublasDgemv(
 #endif
-	handle, side, uplo, transa, diag, &m, &n,
-	ccast(&alpha), ccast(a), &lda, cast(b), &ldb);
+	handle, trans, m, n,
+	alpha, A, lda, x, incx,
+	beta, y, incy);
 }
 
-// LAPACK
-// Compute LU factorization of general matrix using partial pivoting with row interchanges
-static inline void xgetrf(const int m, const int n, num* a,
-		const int lda, int* ipiv, int* info)
-{
-#ifdef USE_CPLX
-	zgetrf(
-#else
-	dgetrf(
-#endif
-	&m, &n, cast(a), &lda, ipiv, info);
-}
+// // triangular matrix - general matrix product, level 3 BLAS
+// static inline void xtrmm(cublasHandle_t handle, 
+// 		cublasSideMode_t side, cublasFillMode_t uplo, 
+// 		const char *transa, const char *diag,
+// 		const int m, const int n,
+// 		const num alpha, const num *a, const int lda,
+// 		num *b, const int ldb)
+// {
+// #ifdef USE_CPLX
+// 	ztrmm(
+// #else
+// 	dtrmm(
+// #endif
+// 	handle, side, uplo, transa, diag, &m, &n,
+// 	ccast(&alpha), ccast(a), &lda, cast(b), &ldb);
+// }
 
-// LAPACK
-// Compute inverse of general matrix using LU factorization provided by xgetrf
-static inline void xgetri(const int n, num* a, const int lda, const int* ipiv,
-		num* work, const int lwork, int* info)
-{
-#ifdef USE_CPLX
-	zgetri(
-#else
-	dgetri(
-#endif
-	&n, cast(a), &lda, ipiv, cast(work), &lwork, info);
-}
+// // LAPACK
+// // Compute LU factorization of general matrix using partial pivoting with row interchanges
+// static inline void xgetrf(const int m, const int n, num* a,
+// 		const int lda, int* ipiv, int* info)
+// {
+// #ifdef USE_CPLX
+// 	zgetrf(
+// #else
+// 	dgetrf(
+// #endif
+// 	&m, &n, cast(a), &lda, ipiv, info);
+// }
 
-// LAPACK
-// Solve linear equation with general matrix using LU factorization provided by zgetrf
-static inline void xgetrs(const char* trans, const int n, const int nrhs,
-		const num* a, const int lda, const int* ipiv,
-		num* b, const int ldb, int* info)
-{
-#ifdef USE_CPLX
-	zgetrs(
-#else
-	dgetrs(
-#endif
-	trans, &n, &nrhs, ccast(a), &lda, ipiv, cast(b), &ldb, info);
-}
+// // LAPACK
+// // Compute inverse of general matrix using LU factorization provided by xgetrf
+// static inline void xgetri(const int n, num* a, const int lda, const int* ipiv,
+// 		num* work, const int lwork, int* info)
+// {
+// #ifdef USE_CPLX
+// 	zgetri(
+// #else
+// 	dgetri(
+// #endif
+// 	&n, cast(a), &lda, ipiv, cast(work), &lwork, info);
+// }
 
-// LAPACK
-// QR factorization of general matrix using column pivoting
-static inline void xgeqp3(const int m, const int n, num* a, const int lda, int* jpvt, num* tau,
-		num* work, const int lwork, double* rwork, int* info)
-{
-#ifdef USE_CPLX
-	zgeqp3(&m, &n, cast(a), &lda, jpvt, cast(tau),
-	cast(work), &lwork, rwork, info);
-#else
-	dgeqp3(&m, &n, cast(a), &lda, jpvt, cast(tau),
-	cast(work), &lwork, info); // rwork not used
-#endif
-}
+// // LAPACK
+// // Solve linear equation with general matrix using LU factorization provided by zgetrf
+// static inline void xgetrs(const char* trans, const int n, const int nrhs,
+// 		const num* a, const int lda, const int* ipiv,
+// 		num* b, const int ldb, int* info)
+// {
+// #ifdef USE_CPLX
+// 	zgetrs(
+// #else
+// 	dgetrs(
+// #endif
+// 	trans, &n, &nrhs, ccast(a), &lda, ipiv, cast(b), &ldb, info);
+// }
+
+// // LAPACK
+// // QR factorization of general matrix using column pivoting
+// static inline void xgeqp3(const int m, const int n, num* a, const int lda, int* jpvt, num* tau,
+// 		num* work, const int lwork, double* rwork, int* info)
+// {
+// #ifdef USE_CPLX
+// 	zgeqp3(&m, &n, cast(a), &lda, jpvt, cast(tau),
+// 	cast(work), &lwork, rwork, info);
+// #else
+// 	dgeqp3(&m, &n, cast(a), &lda, jpvt, cast(tau),
+// 	cast(work), &lwork, info); // rwork not used
+// #endif
+// }
 
 
-// LAPACK
-// QR factorization of general matrix without pivoting
-static inline void xgeqrf(const int m, const int n, num* a, const int lda, num* tau,
-		num* work, const int lwork, int* info)
-{
-#ifdef USE_CPLX
-	zgeqrf(
-#else
-	dgeqrf(
-#endif
-	&m, &n, cast(a), &lda, cast(tau), cast(work), &lwork, info);
-}
+// // LAPACK
+// // QR factorization of general matrix without pivoting
+// static inline void xgeqrf(const int m, const int n, num* a, const int lda, num* tau,
+// 		num* work, const int lwork, int* info)
+// {
+// #ifdef USE_CPLX
+// 	zgeqrf(
+// #else
+// 	dgeqrf(
+// #endif
+// 	&m, &n, cast(a), &lda, cast(tau), cast(work), &lwork, info);
+// }
 
-// LAPACK
-// Multiplies a real/complex matrix by the orthogonal/unitary matrix Q 
-// of the QR factorization formed by xgeqrf or xgeqp3
-static inline void xunmqr(const char* side, const char* trans,
-		const int m, const int n, const int k, const num* a,
-		const int lda, const num* tau, num* c,
-		const int ldc, num* work, const int lwork, int* info)
-{
-#ifdef USE_CPLX
-	zunmqr(side, trans,
-#else
-	dormqr(side, trans[0] == 'C' ? "T" : trans,
-#endif
-	&m, &n, &k, ccast(a), &lda, ccast(tau),
-	cast(c), &ldc, cast(work), &lwork, info);
-}
+// // LAPACK
+// // Multiplies a real/complex matrix by the orthogonal/unitary matrix Q 
+// // of the QR factorization formed by xgeqrf or xgeqp3
+// static inline void xunmqr(const char* side, const char* trans,
+// 		const int m, const int n, const int k, const num* a,
+// 		const int lda, const num* tau, num* c,
+// 		const int ldc, num* work, const int lwork, int* info)
+// {
+// #ifdef USE_CPLX
+// 	zunmqr(side, trans,
+// #else
+// 	dormqr(side, trans[0] == 'C' ? "T" : trans,
+// #endif
+// 	&m, &n, &k, ccast(a), &lda, ccast(tau),
+// 	cast(c), &ldc, cast(work), &lwork, info);
+// }
 
-// LAPACK
-// Compute inverse of triangular matrix
-static inline void xtrtri(const char* uplo, const char* diag, const int n,
-		num* a, const int lda, int* info)
-{
-#ifdef USE_CPLX
-	ztrtri(
-#else
-	dtrtri(
-#endif
-	uplo, diag, &n, cast(a), &lda, info);
-}
+// // LAPACK
+// // Compute inverse of triangular matrix
+// static inline void xtrtri(const char* uplo, const char* diag, const int n,
+// 		num* a, const int lda, int* info)
+// {
+// #ifdef USE_CPLX
+// 	ztrtri(
+// #else
+// 	dtrtri(
+// #endif
+// 	uplo, diag, &n, cast(a), &lda, info);
+// }
 
 #undef ccast
 #undef cast
--- src/testlinalg.c
+++ src/testlinalg.c
@@ -1,7 +1,85 @@
-#include "linalg.h"
+//#include "linalg.h"
 #include <stdio.h>
+#include <stdexcept>
+
+
+#include <cublas_v2.h>
+#include <cuda_runtime.h>
+
+// y <- alpha * A * x + beta * y when trans = 'N' or 'n'
+// xgemv(cublasHandle_t handle,
+// 		const cublasOperation_t trans, 
+// 		const int m, const int n,
+// 		const num *alpha, /* host or device pointer */ 
+// 		const num *A, 
+// 		const int lda,
+// 		const num *x, 
+// 		const int incx,
+// 		const num *beta, /* host or device pointer */
+// 		num *y, 
+// 		const int incy);
+
+#define m 2
+#define n 2
+
+// CUDA API error checking
+#define CUDA_CHECK(err)                                                                            \
+    do {                                                                                           \
+        cudaError_t err_ = (err);                                                                  \
+        if (err_ != cudaSuccess) {                                                                 \
+            printf("CUDA error %d at %s:%d\n", err_, __FILE__, __LINE__);                     \
+            throw std::runtime_error("CUDA error");                                                \
+        }                                                                                          \
+    } while (0)
+
+// cublas API error checking
+#define CUBLAS_CHECK(err)                                                                          \
+    do {                                                                                           \
+        cublasStatus_t err_ = (err);                                                               \
+        if (err_ != CUBLAS_STATUS_SUCCESS) {                                                       \
+            printf("cublas error %d at %s:%d\n", err_, __FILE__, __LINE__);                   \
+            throw std::runtime_error("cublas error");                                              \
+        }                                                                                          \
+    } while (0)
+
+
+
+
+typedef double data_type;
+
+
+
+
 
 int main(){
     printf("hello world\n");
+
+    cublasHandle_t cublasH = NULL;
+    cudaStream_t stream = NULL;
+
+    // const int m = 2;
+    // const int n = 2;
+    const int lda = m;
+
+    const double A[m*n] = {1,2,3,4};
+    const double x[n] = {5,6};
+    const double alpha = 1.0;
+    const double beta = 0.0;
+    double y[m] = {0,0};
+    const int incx =1;
+    const int incy =1;
+
+    data_type *d_A = NULL;
+    data_type *d_x = NULL;
+    data_type *d_y = NULL;
+
+    cublasOperation_t transa = CUBLAS_OP_N;
+    
+
+    printf("size of A: %d\n",sizeof(A));
+    printf("size of alpha: %d\n",sizeof(alpha));
+
+    /* step 1: create cublas handle, bind a stream */
+    CUBLAS_CHECK(cublasCreate(&cublasH));
 
 }
