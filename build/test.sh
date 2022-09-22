63fa6836f43e84400e28319e0aea39a42b10b7b4
Jixun Ding <jxding@stanford.edu>
2022-09-21 23:48:37 -0700

    stop tracking test script

--- build/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo using hdf5 generation file in $DEV
-rm stack* *.h5.log -v
-python3 ${DEV}/util/gen_1band_triangular_hub.py seed=1234 Nfiles=3 Nx=8 Ny=8 overwrite=1 tp=-0.25 L=80 dt=0.05 nflux=1 period_uneqlt=0 meas_energy_corr=1 n_sweep_warm=10 n_sweep_meas=10 checkpoint_every=5 &&
-python3 ${DEV}/util/push.py stack *.h5
