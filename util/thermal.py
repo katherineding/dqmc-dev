ad8f8ad78a696dea303a456cdd974e34fe4089f9
Jixun Ding <jxding@stanford.edu>
2022-11-26 00:03:44 -0800

    [bugfix] adjust thermal.py analysis script for removal of thermal phase defines introduced in 16e84cc995a1d5f8b67804002a5f0e56c64727b1

--- util/thermal.py
+++ util/thermal.py
@@ -10,33 +10,25 @@ if not src+"edwin/util" in sys.path:
 import util #Edwin's util file
 import data_analysis as da
 
-time_slice = 1;
+time_slice = 0;
 
 
 def j2j2_jxding(path):
-
-    U, beta, Nx, Ny, bps, b2ps, nflux, tp, N, L, dt, init_rng= \
+    U, beta, Nx, Ny, bps, b2ps, nflux, tp, N, L, dt= \
         util.load_firstfile(path,"metadata/U","metadata/beta",\
             "metadata/Nx","metadata/Ny","metadata/bps",\
             "metadata/b2ps", "metadata/nflux",\
-            "metadata/t'","params/N","params/L","params/dt","state/init_rng")    
-    ns, s, j2j2 = util.load(path, 
+            "metadata/t'","params/N","params/L","params/dt")    
+    ns, s, j2j2, init_rng = util.load(path, 
         "meas_uneqlt/n_sample", "meas_uneqlt/sign", \
-        "meas_uneqlt/j2j2")
-
+        "meas_uneqlt/j2j2","state/init_rng")
 
     j2j2.shape = -1, L, b2ps, b2ps, Ny, Nx
-    #print(j2j2.shape)
     v_q0 = j2j2.sum((-1,-2))
-    #print(v_q0.shape)
     accum = 0;
-    if nflux == 0:  
-        pre_array= [1+2*tp**2, 1+2*tp**2, 2, 2, 2*tp,2*tp,2*tp,2*tp,4*tp,4*tp,tp**2,tp**2]
-    else: 
-        pre_array = [1,1,1,1,tp,tp,tp,tp,tp,tp,tp**2,tp**2]
+    pre_array = [1,1,1,1,tp,tp,tp,tp,tp,tp,tp**2,tp**2]
     for itype in range(b2ps):
         for jtype in range(b2ps):
-            #print(f"itype = {itype}, jtype = {jtype}, corr = {v_q0[0,0,itype,jtype]}");
             accum += pre_array[itype]*pre_array[jtype]*v_q0[:,time_slice,itype,jtype]
 
     print("j2j2 jxding",accum); 
@@ -44,29 +36,25 @@ def j2j2_jxding(path):
     return accum
 
 def j2j2_wen(path):
-    U, beta, Nx, Ny, bps, b2ps, tp, N, L, dt, init_rng= \
+    U, beta, Nx, Ny, bps, b2ps, tp, N, L, dt= \
         util.load_firstfile(path,"metadata/U","metadata/beta",\
             "metadata/Nx","metadata/Ny","metadata/bps",\
             "metadata/b2ps", \
-            "metadata/t'","params/N","params/L","params/dt","state/init_rng")    
-    ns, s, j2j2 = util.load(path, 
+            "metadata/t'","params/N","params/L","params/dt")    
+    ns, s, j2j2, init_rng = util.load(path, 
         "meas_uneqlt/n_sample", "meas_uneqlt/sign", \
-        "meas_uneqlt/j2j2")
-
+        "meas_uneqlt/j2j2","state/init_rng")
 
     j2j2.shape = -1, L, b2ps, b2ps, Ny, Nx
-    #print(j2j2.shape)
     v_q0 = j2j2.sum((-1,-2))
 
     pre_array = [4*tp, 4*tp,  2,  2,  1+2*tp**2, 1+2*tp**2, 2*tp, 2*tp, tp**2, 2*tp, 2*tp, tp**2]
-    #print(v_q0.shape)
     accum = 0;
     for itype in range(b2ps):
         for jtype in range(b2ps):
-            #print(f"wen, itype = {itype}, jtype = {jtype}, corr = {v_q0[0,time_slice,itype,jtype]}");
             accum += pre_array[itype]*pre_array[jtype]*v_q0[:,time_slice,itype,jtype]
 
-    print("j2j2 wen",accum); 
+    print("j2j2 wen   ",accum); 
 
     return accum
 
@@ -81,26 +69,21 @@ def jnj2_jxding(path):
         "meas_uneqlt/n_sample", "meas_uneqlt/sign", \
         "meas_uneqlt/jnj2")
 
-    jnj2.shape = -1, L,  b2ps, bps,Ny, Nx
+    jnj2.shape = -1, L, b2ps, bps,Ny, Nx
 
     #sum over all locations i.e. take q=0
     v_q0 = jnj2.sum((-1,-2))  #shape = -1, L, bps, hop2ps
 
     #print(v_q0.shape)
     accum = 0;
-    if nflux == 0:  
-        pre_array= [1+2*tp**2, 1+2*tp**2, 2, 2, 2*tp,2*tp,2*tp,2*tp,4*tp,4*tp,tp**2,tp**2]
-    else: 
-        pre_array = [1,1,1,1,tp,tp,tp,tp,tp,tp,tp**2,tp**2]
-    #pre_array = [1+2*tp**2, 1+2*tp**2, 2, 2, 2*tp,2*tp,2*tp,2*tp,4*tp,4*tp,tp**2,tp**2]
+    pre_array = [1,1,1,1,tp,tp,tp,tp,tp,tp,tp**2,tp**2]
     pre_array_bond = [1,1,tp,tp]
         
     for itype in range(b2ps):
         for jtype in range(bps):
-            #print(f"itype = {itype}, jtype = {jtype}, corr = {v_q0[0,0,itype,jtype]}");
             accum += pre_array[itype]*pre_array_bond[jtype]*v_q0[:,time_slice,itype,jtype]
 
-    print("jnj2 jxding smart",accum); 
+    print("jnj2 jxding",accum); 
 
     return accum
 
@@ -115,21 +98,16 @@ def jnj2_wen(path):
         "meas_uneqlt/n_sample", "meas_uneqlt/sign", \
         "meas_uneqlt/jnj")
 
-
     jnj2.shape = -1, L, b2ps, bps, Ny, Nx
-    #print(jnj2.shape)
     v_q0 = jnj2.sum((-1,-2))
-    #print(v_q0.shape)
     accum = 0;
     pre_array = [4*tp, 4*tp,  2,  2,  1+2*tp**2, 1+2*tp**2, 2*tp, 2*tp, tp**2, 2*tp, 2*tp, tp**2]
     pre_array_bond = [1,1,tp,tp]
-    #print(pre_array)
     for itype in range(b2ps):
         for jtype in range(bps):
-            #print(f"wen, itype = {itype}, jtype = {jtype}, corr = {v_q0[0,time_slice,itype,jtype]}");
             accum += pre_array[itype]*pre_array_bond[jtype]*v_q0[:,time_slice,itype,jtype]
 
-    print("jnj2 wen",accum); 
+    print("jnj2 wen   ",accum); 
 
     return accum
 
@@ -147,21 +125,16 @@ def jj2_jxding(path):
     jj2.shape = -1, L,  b2ps, bps,Ny, Nx
 
     #sum over all locations i.e. take q=0
-    v_q0 = jj2.sum((-1,-2))  #shape = -1, L, bps, hop2ps
+    v_q0 = jj2.sum((-1,-2))  #shape = -1, L, b2ps, bps
 
-    #print(v_q0.shape)
     accum = 0;
-    if nflux == 0:  
-        pre_array= [1+2*tp**2, 1+2*tp**2, 2, 2, 2*tp,2*tp,2*tp,2*tp,4*tp,4*tp,tp**2,tp**2]
-    else: 
-        pre_array = [1,1,1,1,tp,tp,tp,tp,tp,tp,tp**2,tp**2]
+    pre_array = [1,1,1,1,tp,tp,tp,tp,tp,tp,tp**2,tp**2]
     pre_array_bond = [1,1,tp,tp]
     for itype in range(b2ps):
         for jtype in range(bps):
-            #print(f"itype = {itype}, jtype = {jtype}, corr = {v_q0[0,0,itype,jtype]}");
             accum += pre_array[itype]*pre_array_bond[jtype]*v_q0[:,time_slice,itype,jtype]
 
-    print("jj2 jxding smart",accum); 
+    print("jj2 jxding",accum); 
 
     return accum
 
@@ -179,18 +152,14 @@ def jj2_wen(path):
 
 
     j2j2.shape = -1, L, b2ps, b2ps, Ny, Nx
-    #print(j2j2.shape)
     v_q0 = j2j2.sum((-1,-2))
-    #print(v_q0.shape)
     accum = 0;
     pre_array_bond = [1,1,tp,tp]
     pre_array = [4*tp, 4*tp,  2,  2,  1+2*tp**2, 1+2*tp**2, 2*tp, 2*tp, tp**2, 2*tp, 2*tp, tp**2]
-    #print(pre_array)
     for itype in range(b2ps):
         for jtype in range(0,4):
-            #print(f"wen, itype = {itype}, jtype = {jtype}, corr = {v_q0[0,time_slice,itype,jtype]}");
             accum += pre_array[itype]*pre_array_bond[jtype]*v_q0[:,time_slice,itype,jtype]
 
-    print("jj2 wen",accum); 
+    print("jj2 wen   ",accum); 
 
     return accum
