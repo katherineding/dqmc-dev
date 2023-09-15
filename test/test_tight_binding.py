ad4cd5d671ee4101a4d5aad06a72a59b77996ab3
Jixun Ding <dismantled.world@gmail.com>
2023-09-15 02:31:43 -0700

    more python tests

--- test/test_tight_binding.py
+++ test/test_tight_binding.py
@@ -13,10 +13,12 @@ class TestKagome:
         tp = 0
         tpp = 0
         nflux = 0
-        Hmat, Peierls = tb.H_periodic_kagome(self.Nx, self.Ny, t=self.t, tp=tp, tpp=tpp, nflux=nflux, alpha=1 / 2)
+        Hmat, Peierls = tb.H_periodic_kagome(
+            self.Nx, self.Ny, t=self.t, tp=tp, tpp=tpp, nflux=nflux, alpha=1 / 2
+        )
 
-        assert Hmat.shape == (3*self.Nx*self.Ny,3*self.Nx*self.Ny)
-        assert Hmat.dtype==np.complex128
+        assert Hmat.shape == (3 * self.Nx * self.Ny, 3 * self.Nx * self.Ny)
+        assert Hmat.dtype == np.complex128
         assert np.max(Peierls.imag) < 1e-10
         assert np.max(np.abs(Hmat.imag)) < 1e-10
 
@@ -25,50 +27,148 @@ class TestKagome:
         tpp = 0
         nflux = 1
         with pytest.raises(Exception):
-            Hmat, Peierls = tb.H_periodic_kagome(self.Nx, self.Ny, t=self.t, tp=tp, tpp=tpp, nflux=nflux, alpha=1 / 2)
-        
+            Hmat, Peierls = tb.H_periodic_kagome(
+                self.Nx, self.Ny, t=self.t, tp=tp, tpp=tpp, nflux=nflux, alpha=1 / 2
+            )
 
     def test_tpp(self):
         tp = 1
         tpp = 0.3
         nflux = 1
         with pytest.raises(Exception):
-            Hmat, Peierls = tb.H_periodic_kagome(self.Nx, self.Ny, t=self.t, tp=tp, tpp=tpp, nflux=nflux, alpha=1 / 2)
+            Hmat, Peierls = tb.H_periodic_kagome(
+                self.Nx, self.Ny, t=self.t, tp=tp, tpp=tpp, nflux=nflux, alpha=1 / 2
+            )
 
 
-class TestOther:
-    Nx = 10
-    Ny = 10
+class TestHoneycomb:
+    Nx = 4
+    Ny = 4
     t = 1
 
-    def test1(self):
-        nflux = 1
+    def test_0(self):
         tp = 0
-        Hmat, _ = tb.H_periodic_triangular(self.Nx, self.Ny, t=self.t, tp=tp, nflux=nflux, alpha=1 / 2)
-        evals, V = np.linalg.eigh(Hmat)
+        tpp = 0
+        nflux = 0
+        Hmat, Peierls = tb.H_periodic_honeycomb(
+            self.Nx, self.Ny, t=self.t, tp=tp, tpp=tpp, nflux=nflux, alpha=1 / 2
+        )
 
-    def test2(self):
+        assert Hmat.shape == (2 * self.Nx * self.Ny, 2 * self.Nx * self.Ny)
+        assert Hmat.dtype == np.complex128
+        assert np.max(Peierls.imag) < 1e-10
+        assert np.max(np.abs(Hmat.imag)) < 1e-10
+
+    def test_tp(self):
         tp = 1
         tpp = 0
         nflux = 1
         with pytest.raises(Exception):
-            Hmat, Peierls = tb.H_periodic_kagome(self.Nx, self.Ny, t=self.t, tp=tp, tpp=tpp, nflux=nflux, alpha=1 / 2)
-        
-        #evals, V = np.linalg.eigh(Hmat)
+            Hmat, Peierls = tb.H_periodic_honeycomb(
+                self.Nx, self.Ny, t=self.t, tp=tp, tpp=tpp, nflux=nflux, alpha=1 / 2
+            )
 
-    def test3(self):
-        Ncell = self.Nx * self.Ny
+    def test_tpp(self):
+        tp = 1
+        tpp = 0.3
+        nflux = 1
+        with pytest.raises(Exception):
+            Hmat, Peierls = tb.H_periodic_honeycomb(
+                self.Nx, self.Ny, t=self.t, tp=tp, tpp=tpp, nflux=nflux, alpha=1 / 2
+            )
+
+
+class TestSquare:
+    Nx = 6
+    Ny = 6
+    t = 1
+
+    def test_0(self):
         tp = 0
         tpp = 0
-        alpha = 1 / 2
-        geometry = "triangular"
-        nf_arr = np.arange(0, Ncell + 1)
-        nF = nf_arr.shape[0]
+        nflux = 0
+        Hmat, Peierls = tb.H_periodic_square(
+            self.Nx, self.Ny, t=self.t, tp=tp, tpp=tpp, nflux=nflux, alpha=1 / 2
+        )
 
-        beta = 5
-        mu = -3
-        nflux = 2
-        print(nflux / Ncell)
-        Hmat, _ = tb.H_periodic_square(self.Nx, self.Ny, t=self.t, tp=0, nflux=nflux, alpha=1 / 2)
-        evals, V = np.linalg.eigh(Hmat)
+        assert Hmat.shape == (1 * self.Nx * self.Ny, 1 * self.Nx * self.Ny)
+        assert Hmat.dtype == np.complex128
+        assert np.max(Peierls.imag) < 1e-10
+        assert np.max(np.abs(Hmat.imag)) < 1e-10
 
+    def test_tp(self):
+        tp = 1
+        tpp = 0
+        nflux = 1
+        Hmat, Peierls = tb.H_periodic_square(
+            self.Nx, self.Ny, t=self.t, tp=tp, tpp=tpp, nflux=nflux, alpha=1 / 2
+        )
+
+    def test_tpp(self):
+        tp = 1
+        tpp = 0.3
+        nflux = 1
+        Hmat, Peierls = tb.H_periodic_square(
+            self.Nx, self.Ny, t=self.t, tp=tp, tpp=tpp, nflux=nflux, alpha=1 / 2
+        )
+
+    def test_size(self):
+        Nx = 4
+        Ny = 4
+        tp = 1
+        tpp = 0.3
+        nflux = 1
+        with pytest.raises(Exception):
+            # This lattice is too small to accomodate tpp
+            Hmat, Peierls = tb.H_periodic_square(
+                Nx, Ny, t=self.t, tp=tp, tpp=tpp, nflux=nflux, alpha=1 / 2
+            )
+
+
+class TestTriangular:
+    Nx = 6
+    Ny = 6
+    t = 1
+
+    def test_0(self):
+        tp = 0
+        tpp = 0
+        nflux = 0
+        Hmat, Peierls = tb.H_periodic_triangular(
+            self.Nx, self.Ny, t=self.t, tp=tp, tpp=tpp, nflux=nflux, alpha=1 / 2
+        )
+
+        assert Hmat.shape == (1 * self.Nx * self.Ny, 1 * self.Nx * self.Ny)
+        assert Hmat.dtype == np.complex128
+        assert np.max(Peierls.imag) < 1e-10
+        assert np.max(np.abs(Hmat.imag)) < 1e-10
+
+    def test_tp(self):
+        tp = 1
+        tpp = 0
+        nflux = 1
+        with pytest.raises(Exception):
+            Hmat, Peierls = tb.H_periodic_triangular(
+                self.Nx, self.Ny, t=self.t, tp=tp, tpp=tpp, nflux=nflux, alpha=1 / 2
+            )
+
+    def test_tpp(self):
+        tp = 1
+        tpp = 0.3
+        nflux = 1
+        with pytest.raises(Exception):
+            Hmat, Peierls = tb.H_periodic_triangular(
+                self.Nx, self.Ny, t=self.t, tp=tp, tpp=tpp, nflux=nflux, alpha=1 / 2
+            )
+
+    def test_size(self):
+        Nx = 4
+        Ny = 4
+        tp = 1
+        tpp = 0.3
+        nflux = 1
+        with pytest.raises(Exception):
+            # This lattice is too small to accomodate tpp
+            Hmat, Peierls = tb.H_periodic_triangular(
+                Nx, Ny, t=self.t, tp=tp, tpp=tpp, nflux=nflux, alpha=1 / 2
+            )
