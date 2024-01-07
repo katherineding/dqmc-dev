864ffe913b21fbb731012ebf4c9c5350316f5b99
Jixun Ding <dismantled.world@gmail.com>
2024-01-06 19:25:31 -0800

    add more thermal correlator checks

--- util/makeref.py
+++ util/makeref.py
@@ -14,34 +14,59 @@ seed = 1234
 
 def gen_ref():
     for geometry in geometry_list:
-
         if geometry == "square":
-            tp=-0.25
+            tp = -0.25
         else:
             tp = 0
         ghub.create_batch(
             prefix=f"{ghub.hash_short}_{geometry}_{seed}_nflux0",
             geometry=geometry,
-            Nx = 4, Ny = 4, dt = 0.1, L = 40, nflux=0, period_uneqlt=2,
-            meas_bond_corr = 1, meas_energy_corr = 1,
-            meas_chiral=1, meas_thermal=1, meas_2bond_corr=1,
-            trans_sym =1, Nfiles=3, tp = tp,
-            seed=seed, overwrite=1, n_sweep_warm=20, n_sweep_meas=20
+            Nx=4,
+            Ny=4,
+            dt=0.1,
+            L=40,
+            nflux=0,
+            period_uneqlt=2,
+            meas_bond_corr=1,
+            meas_energy_corr=1,
+            meas_chiral=1,
+            meas_thermal=1,
+            meas_2bond_corr=1,
+            trans_sym=1,
+            Nfiles=3,
+            tp=tp,
+            seed=seed,
+            overwrite=1,
+            n_sweep_warm=20,
+            n_sweep_meas=20,
         )
         ghub.create_batch(
             prefix=f"{ghub.hash_short}_{geometry}_{seed}_nflux3",
             geometry=geometry,
-            Nx = 4, Ny = 4, dt = 0.1, L = 40, nflux=3, period_uneqlt=2,
-            meas_bond_corr = 1, meas_energy_corr = 1,
-            meas_chiral=1, meas_thermal=1, meas_2bond_corr=1,
-            trans_sym =1, Nfiles=3, tp = tp,
-            seed=seed, overwrite=1, n_sweep_warm=20, n_sweep_meas=20
+            Nx=4,
+            Ny=4,
+            dt=0.1,
+            L=40,
+            nflux=3,
+            period_uneqlt=2,
+            meas_bond_corr=1,
+            meas_energy_corr=1,
+            meas_chiral=1,
+            meas_thermal=1,
+            meas_2bond_corr=1,
+            trans_sym=1,
+            Nfiles=3,
+            tp=tp,
+            seed=seed,
+            overwrite=1,
+            n_sweep_warm=20,
+            n_sweep_meas=20,
         )
-
 
 
 def main():
     gen_ref()
+
 
 if __name__ == "__main__":
     main()
--- util/thermal.py
+++ util/thermal.py
@@ -37,7 +37,7 @@ def j2j2_jxding(path):
                 pre_array[itype] * pre_array[jtype] * v_q0[:, time_slice, itype, jtype]
             )
 
-    print("j2j2 jxding", accum)
+    print("j2j2 jxding", accum.real, accum.imag)
 
     return accum
 
@@ -88,7 +88,7 @@ def j2j2_wen(path):
                 pre_array[itype] * pre_array[jtype] * v_q0[:, time_slice, itype, jtype]
             )
 
-    print("j2j2 wen   ", accum)
+    print("j2j2 wen   ", accum.real, accum.imag)
 
     return accum
 
@@ -131,7 +131,7 @@ def jnj2_jxding(path):
                 * v_q0[:, time_slice, itype, jtype]
             )
 
-    print("jnj2 jxding", accum)
+    print("jnj2 jxding", accum.real, accum.imag)
 
     return accum
 
@@ -181,7 +181,100 @@ def jnj2_wen(path):
                 * v_q0[:, time_slice, itype, jtype]
             )
 
-    print("jnj2 wen   ", accum)
+    print("jnj2 wen   ", accum.real, accum.imag)
+
+    return accum
+
+
+def j2jn_jxding(path):
+    U, beta, Nx, Ny, bps, b2ps, nflux, tp, N, L, dt, init_rng = util.load_firstfile(
+        path,
+        "metadata/U",
+        "metadata/beta",
+        "metadata/Nx",
+        "metadata/Ny",
+        "metadata/bps",
+        "metadata/b2ps",
+        "metadata/nflux",
+        "metadata/t'",
+        "params/N",
+        "params/L",
+        "params/dt",
+        "state/init_rng",
+    )
+    ns, s, j2jn = util.load(
+        path, "meas_uneqlt/n_sample", "meas_uneqlt/sign", "meas_uneqlt/j2jn"
+    )
+
+    j2jn.shape = -1, L, bps, b2ps, Ny, Nx
+
+    # sum over all locations i.e. take q=0
+    v_q0 = j2jn.sum((-1, -2))  # shape = -1, L, bps, hop2ps
+
+    # print(v_q0.shape)
+    accum = 0
+    pre_array = [1, 1, 1, 1, tp, tp, tp, tp, tp, tp, tp**2, tp**2]
+    pre_array_bond = [1, 1, tp, tp]
+
+    for itype in range(bps):
+        for jtype in range(b2ps):
+            accum += (
+                pre_array_bond[itype]
+                * pre_array[jtype]
+                * v_q0[:, time_slice, itype, jtype]
+            )
+
+    print("j2jn jxding", accum.real, accum.imag)
+
+    return accum
+
+
+def j2jn_wen(path):
+    U, beta, Nx, Ny, bps, b2ps, tp, N, L, dt, init_rng = util.load_firstfile(
+        path,
+        "metadata/U",
+        "metadata/beta",
+        "metadata/Nx",
+        "metadata/Ny",
+        "metadata/bps",
+        "metadata/b2ps",
+        "metadata/t'",
+        "params/N",
+        "params/L",
+        "params/dt",
+        "state/init_rng",
+    )
+    ns, s, j2jn = util.load(
+        path, "meas_uneqlt/n_sample", "meas_uneqlt/sign", "meas_uneqlt/jjn"
+    )
+
+    j2jn.shape = -1, L, bps, b2ps, Ny, Nx
+    v_q0 = j2jn.sum((-1, -2))
+    accum = 0
+    pre_array = [
+        4 * tp,
+        4 * tp,
+        2,
+        2,
+        1 + 2 * tp**2,
+        1 + 2 * tp**2,
+        2 * tp,
+        2 * tp,
+        tp**2,
+        2 * tp,
+        2 * tp,
+        tp**2,
+    ]
+    pre_array_bond = [1, 1, tp, tp]
+    for itype in range(bps):
+        for jtype in range(b2ps):
+            accum += (
+                pre_array_bond[itype]
+                * pre_array[jtype]
+                * v_q0[:, time_slice, itype, jtype]
+            )
+
+    print("j2jn wen   ", accum.real, accum.imag)
 
     return accum
 
@@ -222,7 +315,7 @@ def jj2_jxding(path):
                 * v_q0[:, time_slice, itype, jtype]
             )
 
-    print("jj2 jxding", accum)
+    print("jj2 jxding", accum.real, accum.imag)
 
     return accum
 
@@ -272,6 +365,97 @@ def jj2_wen(path):
                 * v_q0[:, time_slice, itype, jtype]
             )
 
-    print("jj2 wen   ", accum)
+    print("jj2 wen   ", accum.real, accum.imag)
+
+    return accum
+
+
+def j2j_jxding(path):
+    U, beta, Nx, Ny, bps, b2ps, nflux, tp, N, L, dt, init_rng = util.load_firstfile(
+        path,
+        "metadata/U",
+        "metadata/beta",
+        "metadata/Nx",
+        "metadata/Ny",
+        "metadata/bps",
+        "metadata/b2ps",
+        "metadata/nflux",
+        "metadata/t'",
+        "params/N",
+        "params/L",
+        "params/dt",
+        "state/init_rng",
+    )
+    ns, s, j2j = util.load(
+        path, "meas_uneqlt/n_sample", "meas_uneqlt/sign", "meas_uneqlt/j2j"
+    )
+
+    j2j.shape = -1, L, bps, b2ps, Ny, Nx
+
+    # sum over all locations i.e. take q=0
+    v_q0 = j2j.sum((-1, -2))  # shape = -1, L, b2ps, bps
+
+    accum = 0
+    pre_array = [1, 1, 1, 1, tp, tp, tp, tp, tp, tp, tp**2, tp**2]
+    pre_array_bond = [1, 1, tp, tp]
+    for itype in range(bps):
+        for jtype in range(b2ps):
+            accum += (
+                pre_array_bond[itype]
+                * pre_array[jtype]
+                * v_q0[:, time_slice, itype, jtype]
+            )
+
+    print("j2j jxding", accum.real, accum.imag)
+
+    return accum
+
+
+def j2j_wen(path):
+    U, beta, Nx, Ny, bps, b2ps, tp, N, L, dt, init_rng = util.load_firstfile(
+        path,
+        "metadata/U",
+        "metadata/beta",
+        "metadata/Nx",
+        "metadata/Ny",
+        "metadata/bps",
+        "metadata/b2ps",
+        "metadata/t'",
+        "params/N",
+        "params/L",
+        "params/dt",
+        "state/init_rng",
+    )
+    ns, s, j2j2 = util.load(
+        path, "meas_uneqlt/n_sample", "meas_uneqlt/sign", "meas_uneqlt/j2j2"
+    )
+
+    j2j2.shape = -1, L, b2ps, b2ps, Ny, Nx
+    v_q0 = j2j2.sum((-1, -2))
+    accum = 0
+    pre_array_bond = [1, 1, tp, tp]
+    pre_array = [
+        4 * tp,
+        4 * tp,
+        2,
+        2,
+        1 + 2 * tp**2,
+        1 + 2 * tp**2,
+        2 * tp,
+        2 * tp,
+        tp**2,
+        2 * tp,
+        2 * tp,
+        tp**2,
+    ]
+    for itype in range(0, 4):
+        for jtype in range(b2ps):
+            accum += (
+                pre_array_bond[itype]
+                * pre_array[jtype]
+                * v_q0[:, time_slice, itype, jtype]
+            )
+
+    print("j2j wen   ", accum.real, accum.imag)
 
     return accum
